2414295503a1ced873543a0970d19dd56a125f99
Mayank Dandriyal <mayankdandriyal0202@gmail.com>
2024-06-19 12:26:35 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,50 @@
+# Q&A Chatbot
+#from langchain.llms import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+
+import google.generativeai as genai
+
+
+api = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api)
+
+## Function to load OpenAI model and get respones
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response =chat.send_message(question,stream=True)
+    return response
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+
+st.header("Gemini Application based chatbot")
+st.subheader("By tech works")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask any question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    for chunk in response:
+        print(st.write(chunk.text))
+        #print("_"*80)
+    
+    #st.write(chat.history)
+
+
